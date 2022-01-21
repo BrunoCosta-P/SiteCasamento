04c1e9993fa4060fa18ae357720405f39acce5e2
Bruno Costa Pinto <brunocosta524@hotmail.com>
2022-01-21 08:25:44 -0300

    add img carousel, e criado estrutura base do carousel

--- js/main.js
+++ js/main.js
@@ -45,3 +45,12 @@ window.onload = () => {
 
 }
 
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
